745d629156ed9da0a09203ce7eb049c25c8859cf
dunbuchanan <dunbuchanan@gmail.com>
2025-06-23 00:59:59 -0300

    Update WhatsAppButton.tsx

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -3,15 +3,15 @@ import { FaWhatsapp } from "react-icons/fa";
 import "../styles/WhatsAppButton.css";
 
 const WhatsAppButton: React.FC = () => {
-  const phoneNumber = "376665756";
-  const message = "Hola! Me contacto desde la página web";
+  const phoneNumber = "376669363";
+  const message = "Hola! Em contacte des de la pàgina web";
   const link = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
     message
   )}`;
 
   return (
     <div className="whatsapp-container">
-      <div className="whatsapp-message">¡Hacenos tu consulta!</div>
+      <div className="whatsapp-message">¡Fes-nos la teva consulta!</div>
       <a
         href={link}
         target="_blank"
