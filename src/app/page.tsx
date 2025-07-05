91f6e763db32c46c16138e43efffb5501be5f443
GMGonzaloMata <gonza.mata2003@gmail.com>
2025-07-05 02:32:55 -0300

    Finalizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
-"use client";
 import React from "react";
 import Logo from "../components/Logo";
 import Indumentaria from "../components/Indumentaria";
 import WhatsAppButton from "@/components/WhatsAppButton";
 import Formulario from "../components/Formulario";
-import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
   return (
     <div className="app">
       <Logo />
